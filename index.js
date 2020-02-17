6ed43aa7d9ed54df37b04ccd7a52e8303e94254b
Naveen-HN <hnnaveen93@gmail.com>
2020-02-17 15:18:58 -0500

    Added another state ice_cream

--- index.js
+++ index.js
@@ -4,18 +4,26 @@ const createStore = redux.createStore;
 
 
 const BUY_CAKE = 'BUY_CAKE';
-
+const BUY_ICECREAM = 'BUY_ICECREAM';
 
 function buy_cake() {
     return {
         type: BUY_CAKE,
-        info: "First redux action"
+        info: "buying cake"
+    }
+}
+
+function buy_ice_cream() {
+    return {
+        type: BUY_ICECREAM,
+        info: 'buying ice cream'
     }
 }
 
 
 const initialState = {
-    numOfCake: 10
+    numOfCake: 10,
+    numOfIceCream: 20
 }
 
 const reducer = (state = initialState, action) => {
@@ -25,14 +33,24 @@ const reducer = (state = initialState, action) => {
                 ...state,
                 numOfCake: state.numOfCake - 1
             }
-            default:
-                return state
+            case BUY_ICECREAM:
+                return {
+                    ...state,
+                    numOfIceCream: state.numOfIceCream - 1
+                }
+                default:
+                    return state
     }
 }
 
 const store = createStore(reducer);
 console.log("Initial State:", store.getState());
 const unsubscribe = store.subscribe(() => console.log("Updated State:", store.getState()));
+store.dispatch(buy_cake());
+store.dispatch(buy_cake());
+store.dispatch(buy_ice_cream());
+store.dispatch(buy_ice_cream());
+store.dispatch(buy_ice_cream());
 store.dispatch(buy_cake());
 store.dispatch(buy_cake());
 store.dispatch(buy_cake());
